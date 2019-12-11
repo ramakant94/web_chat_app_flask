81eebdeebf31f93870176b243c7e540431ac4af4
ramakant94 <43502933+ramakant94@users.noreply.github.com>
2019-12-11 11:16:12 +0530

    static_javaScript_file

---
+++ main.js
@@ -0,0 +1,42 @@
+// console.log(2+3)  //gives output by console.log
+
+ var socket = io();
+    socket.on('connect', function() {
+    	console.log('connection established')
+    }
+    );
+    //     //socket.emit('my event', {data: 'I\'m connected!'});
+    // });
+
+
+function handleClick(){
+	var inputBox = document.getElementById('inputBox')
+	socket.emit('msg', inputBox.value)
+	inputBox.value=''
+}
+
+
+socket.on('push',function(data){
+	// console.log(data)
+	var chatBox = document.getElementById('chatBox')
+	chatBox.innerHTML += data + '<br/>'
+
+})
+
+
+
+
+
+
+
+
+// function handleClick(){
+// 	// alert('hello')
+// 	var url = '/users'
+// 	fetch(url).then(function(res){
+// 		res.json().then(function(data){
+// 			console.log(data)
+// 		})
+// 	})
+
+// }
